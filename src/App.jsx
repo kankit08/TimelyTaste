f8363f62c686e6c540a4775f5405370710c3513b
kankit08 <kankit08@gmail.com>
2026-08-02 16:43:02 +0530

    tailwind is configured

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 const App = () => {
   return (
     <div>
-      <h2>Timely Taste</h2>
+      <h2 className='text-2xl font-bold text-blue-500'>Timely Taste</h2>
     </div>
   )
 }
